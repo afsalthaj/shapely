1ecb1d15a63e71d42f9d6318cbd187e07c7fc330
athaj <afsal.taj06@gmail.com>
2021-08-30 01:08:20 +1000

    Remove pure from InvariantAp

--- project/TypeClasses.scala
+++ project/TypeClasses.scala
@@ -65,7 +65,6 @@ object TypeClasses {
        |package shapely
        |
        |trait InvariantAp[F[_]] {
-       |  def pure[A](a: A): F[A]
        |  def product1[A, B](f: A => B)(g: B => A)(fa: F[A]): F[B]
        |  def product2[A, B, C](f: (A, B) => C)(g: C => (A, B))(fa: F[A], fb: F[B]): F[C]
        |  ${products.mkString("\n")}
