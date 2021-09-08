77f4535400c7baffafe47111bcb9ab25504bbe24
Afsal Thaj <afsal.taj06@gmail.com>
2021-09-09 00:36:38 +1000

    Update Type.scala

--- src/main/scala/Type.scala
+++ src/main/scala/Type.scala
@@ -29,9 +29,8 @@ object TypeClass extends App {
     }
   }
 
-  inline implicit def deriveF1[F[_], A, A1, L1 <: String](implicit f: InvariantAp[F], ev1: F[A1], ev2: TypeClassInspect[F, ignore]): Lazy[F, CaseClass1[A, A1, L1]] = {
-    val annotations = Annotation.annotationOf[A1, ignore].headOption
-    println(annotations)
+  inline implicit def deriveF1[F[_], A, A1, L1 <: String, Annotation](implicit f: InvariantAp[F], ev1: F[A1], ev2: TypeClassInspect[F, Annotation]): Lazy[F, CaseClass1[A, A1, L1]] = {
+    val annotations = Annotation.annotationOf[A1, Annotation].headOption
     val ev11 = ev2.input(None, "afsal", None, ev1)
 
     instance(f.product1[A1, CaseClass1[A, A1, L1]]((a1) => CaseClass1(a1))(a => (a._1))(ev11))
